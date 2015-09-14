4841b5e498c8bfefcbe815cac39c3aa0f052cb46
Nikhil Patil <indieNik@users.noreply.github.com>
2015-09-14 17:19:46 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,22 @@
+package com.psl.sync;
+
+public class Account {
+	
+	private int balance = 0;
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public Account(int bal) {
+		// TODO Auto-generated constructor stub
+		balance = bal;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+	
+}
